2efcbd333efb59ec0ab5154812ef1603ea30dfaa
Tanvi Chandra <tanc@Tanvis-MacBook-Pro.local>
2025-06-19 17:59:36 -0700

    starter code

---
+++ rewards-systems/rewards-system-1/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Greet } from './components/greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
---
+++ rewards-systems/rewards-system-1/src/components/greet.tsx
@@ -0,0 +1,8 @@
+export const Greet = () => {
+  return (
+    <div>
+      <h1>Welcome to the Rewards System!</h1>
+      <p>We are excited to have you here. Let's get started on your journey to earning rewards!</p>
+    </div>
+  );
+}
